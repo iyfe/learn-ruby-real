80bf0f0399be797dbeb65381137ef00d49782014
Ifunanya Ukachukwu <icukachuk@gmail.com>
2020-05-02 16:01:15 -0700

    Initialize Repository

---
+++ lib/iyfe_palindrome.rb
@@ -0,0 +1,6 @@
+require "iyfe_palindrome/version"
+
+module IyfePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iyfe_palindrome/version.rb
@@ -0,0 +1,3 @@
+module IyfePalindrome
+  VERSION = "0.1.0"
+end
